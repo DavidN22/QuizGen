62cff861a1bf33a7e1cb25f681849168870e3f62
david <naymondavid@gmail.com>
2025-06-18 23:33:19 -0400

    Changed mongodb uri for vercel connection

--- backend/dist/db.mongoose.js
+++ backend/dist/db.mongoose.js
@@ -5,6 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.connectDB = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const connectDB = async () => {
     try {
         await mongoose_1.default.connect(process.env.MONGODB_URI || '');
